834a058a794e592b4752d5ea10a3a4a4ad619e8e
James Yaro <onuhjamesyaro@gmail.com>
2022-05-05 23:45:02 +0100

    Routes and middleware

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,36 @@
+
+// Get goals
+// Get /api/goals
+const getGoals = (req, res) => {
+    res.status(200).json({message: 'Get goals'})
+}
+
+// set goal
+// POST /api/goals
+const setGoal = (req, res) => {
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error ('Please add a text field')
+    }
+    res.status(200).json({message: 'Set goal'})
+}
+
+// update goal
+// PUT /api/goals/:id
+const updateGoal = (req, res) => {
+    res.status(200).json({message: `Update goal ${req.params.id}`})
+}
+
+// Delete goal
+// DELETE /api/goals/:id
+const deleteGoal = (req, res) => {
+    res.status(200).json({message: `Delete goal ${req.params.id}`})
+}
+
+
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal,
+}
